bd6ac01ce0415d8b8dc88d72aead45b6c5c54540
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-27 15:38:31 +0000

    modified planet_list function to have a decorator, manage GET and POST requests, and ensure that posted data is valid

--- planets/views.py
+++ planets/views.py
@@ -1,10 +1,22 @@
 from django.http import JsonResponse
 from .models import Planet
 from .serializers import PlanetSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
 def planet_list(request):
 
-    planets = Planet.objects.all()
-    serializer = PlanetSerializer(planets, many=True)
+    if request.method == 'GET':
+        planets = Planet.objects.all()
+        serializer = PlanetSerializer(planets, many=True)
     
-    return JsonResponse({"planets": serializer.data})
+        return JsonResponse({"planets": serializer.data})
+    
+    if request.method == 'POST':
+        serializer = PlanetSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
