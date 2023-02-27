675588064985054f2420f8db66f44c658ea3c765
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-27 12:57:43 +0000

    typo. i spelt Model.Serializer the english way rather than the american way

--- planets/serialisers.py
+++ planets/serialisers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Planet
 
-class PlanetSerialiser(serializers.ModelSerialiser):
+class PlanetSerialiser(serializers.ModelSerializer):
     class Meta:
         model = Planet
         fields = ['id', 'name', 'desciption']
