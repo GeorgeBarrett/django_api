3aeb01c83060971b4876a9889c2456dd714508ba
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-27 12:18:38 +0000

    method created inside models.py that displays the data, in the planets table, in a more readable/detailed way

--- planets/models.py
+++ planets/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Planet(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name + " " + self.description
