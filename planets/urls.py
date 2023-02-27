387c4031f71888a32ef087399cc4bbab4edda6d4
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-27 12:53:31 +0000

    new url path 'planets' added. when this path is entered then the planet_list method in views.py will fire

--- planets/urls.py
+++ planets/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from planets import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('planets/', views.planet_list)
 ]
