6c60869bcab048feda19a29c80c09c26b3d31573
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-28 11:05:12 +0000

    i have imported format_suffix_patterns to be able to view the planets json data through the browser

--- planets/urls.py
+++ planets/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from planets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('planets/', views.planet_list),
     path('planets/<int:id>', views.planet_detail)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
