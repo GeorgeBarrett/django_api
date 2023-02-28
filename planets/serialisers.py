67b0cac8f6459266d4f5193b20b0eb2e9795dbc1
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-28 14:08:47 +0000

    i am removing the duplicate file serialisers.py

--- planets/serialisers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Planet
-
-class PlanetSerialiser(serializers.ModelSerializer):
-    class Meta:
-        model = Planet
-        fields = ['id', 'name', 'desciption']
